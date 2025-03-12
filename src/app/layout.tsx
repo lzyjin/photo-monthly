fb70aac573317aa3cc403a77d20490aa4ead9036
lzyjin <1247y.j@gmail.com>
2025-03-12 22:15:19 +0900

    useSearchParams 빌드에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {jejuGothic, workSans} from "@/lib/fonts";
+import {Suspense} from "react";
 
 export const metadata: Metadata = {
   title: "Photo Monthly",
@@ -17,7 +18,9 @@ export default function RootLayout({
     <html lang="en" className={`${workSans.variable} ${jejuGothic.variable} `}>
       <body className={`font-worksans antialiased w-full max-w-screen-sm min-h-screen mx-auto bg-white 
       sm:border-foreground sm:border-l sm:border-r`}>
-        {children}
+        <Suspense>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
